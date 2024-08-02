2e91aac7888d6e7c347048da0469b20209ecdcef
marton2307 <marton2307@gmail.com>
2024-08-02 08:38:24 -0500

    create proyect

---
+++ finazas/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormPages from './pages/formpages';
+
+function App() {
+  return (
+    <div className="App">
+     <FormPages/>
+    </div>
+  );
+}
+
+export default App;
---
+++ finazas/src/pages/formpages.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Formpages = () => {
+  return (
+    <div>
+      ,fghjsdhgjd
+    </div>
+  )
+}
+
+export default Formpages
